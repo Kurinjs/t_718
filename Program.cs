ec63c7247f053660efb3c3f41e41856cfa880f2d
DESKTOP-7IM6F89\Артур <kurinjs@mail.ru>
2018-10-27 09:14:00 +0300

    fOR fLAVIK

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PattenCommand
+{
+    class Delete
+    {
+        public void StartProcessing()
+        {
+            Console.WriteLine("Это работает!!!1");
+        }
+    }
+    class DeleteCommand : ICommand
+    {
+        Delete delete;
+        public DeleteCommand(Delete m)
+        {
+            delete = m;
+        }
+        public void Excute()
+        {
+            delete.StartProcessing();
+        }
+        public void Undo()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
